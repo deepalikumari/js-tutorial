c914ae817ca070c4b2982f2cc03a59b749e7cdcd
Deepali kumari <deepalikumari1234@gmail.com>
2023-11-14 09:10:17 +0000

    arthematic

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negvlaue = -value
+//console.log(negvlaue); -3
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hwllo"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+console.log( "1" +2);
+console.log(2 + "2");
+console.log("1"+2+2);
+console.log(2+2 + "2");
+//ecma documentation= Toprimitive
+
+// if string is in first everything is treated as string
+// if string in last and no. in first, first operation in no. then string
+console.log(true);
+console.log(+true); //as true is already a boolean operationa nd we are incrementing
+console.log(+"") //return 0
