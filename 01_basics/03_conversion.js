820b09df0a3be81e81cec5aaf14e9cb86b4077d0
Deepali kumari <deepalikumari1234@gmail.com>
2023-11-14 07:10:49 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,20 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof(score)); //as a method representation
+let valueinnum = Number(score)
+console.log(typeof valueinnum);
+
+let score1 = "33acb"
+let valeIN = Number(score1)
+console.log(typeof valeIN);
+console.log(valeIN);
+
+//1 = true
+//0 = false
+//"" = false
+//"deepali" = true
+
+let somenum = 33
+let stringnum = String(somenum)
+console.log(stringnum)
+console.log(typeof stringnum)
