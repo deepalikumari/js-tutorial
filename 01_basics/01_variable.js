056b4e50d2ef484aca8889c4c3077c6d2310af3c
Deepali kumari <deepalikumari1234@gmail.com>
2023-11-14 06:17:27 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "deepali_kumari@srmap.edu.in"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 
+accountEmail = "dk1256@gmail.com"
+accountPassword = "5678235"
+accountCity = "Delhi"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+"use strict"; //treat all js code as newer version
+//alert("hello")
+console.log(7 + 9)
+
