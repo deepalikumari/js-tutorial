7695512067ed77709f4886430e85d5c1a2339501
Deepali kumari <deepalikumari1234@gmail.com>
2023-11-14 13:03:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+console.log(2 > 1)
+console.log("2" > 1); //automatically convert 2 in no.
+
+console.log(null > 0);
+console.log(null >= 0); //true as here nullis converted to 0
+
+console.log(null == 0);
+//undefined will always gives false
+console.log(undefined == 0);
+
+//strictly check , check datatype too
+console.log("2" === 2); // ===(strict check)
+
+
+
+
+
+
+
+
+
+
